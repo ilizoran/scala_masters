317c905441d2a607434cfa6bd4fb98cb57b45fc7
s7npaic <npaic@s7designcreative.com>
2015-10-10 23:50:24 +0200

    calculator done

---
+++ src/main/scala-2.11/Calculator.scala
@@ -0,0 +1,45 @@
+import scala.annotation.tailrec
+
+object Calculator {
+
+  def main(args: Array[String]) {
+    try{
+      val number: Int = io.StdIn.readLine("Enter start number: " ).toInt
+      println(this.calculate(number))
+    }catch{
+      case num: NumberFormatException => println("You must enter a number without decimal point")
+      case _: Exception => println("Error has occurred")
+    }
+
+
+  }
+
+  /**
+   * Calculate result in recursion
+   *
+   * If you not choose = as operator function will go in recursion and ask for next number to calculate with last result,
+   * if you enter wrong operator sum will be applied
+   *
+   * @param x [Int]
+   *
+   * @return [Int]
+   */
+  @tailrec
+  def calculate(x: Int): Int = {
+    val operate = io.StdIn.readLine("Enter operator")
+    if(operate == "="){
+      x
+    }else{
+      val newNumber: Int = io.StdIn.readLine("Enter number").toInt
+      val result: Int = operate match {
+        case "+" => x + newNumber
+        case "-" => x - newNumber
+        case "*" => x * newNumber
+        case "/" => x / newNumber
+        case _   => x + newNumber
+      }
+      return this.calculate(result)
+    }
+  }
+
+}
